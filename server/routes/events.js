70aab2af0aaf05341bc6641779364427a615793d
Moses Ngone <moses@ninelabs.com>
2014-10-15 20:24:06 -0400

    modulating the ui

--- server/routes/events.js
+++ server/routes/events.js
@@ -8,9 +8,24 @@
  * Event Routes
  */
 
+
 module.exports = function(app) {
     app.get('/events', function(req, res) {
-        res.render('events/list',{module:'events-list'});
+        res.render('events/list', {
+            module: 'events'
+        });
     });
+
+    // app.get('/events/:year/:month/:title', function(req, res, next, id) {
+    //     res.render('events/list', {
+    //         module: 'events-list'
+    //     });
+    // });
+
+    // app.get('/events/:year/:month/:title', function(req, res, next, id) {
+    //     res.render('events/list', {
+    //         module: 'events-list'
+    //     });
+    // });
 
 };
